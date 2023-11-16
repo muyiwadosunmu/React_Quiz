53f587b3fb511243605d4b229aa892be967130e7
Dosunmu Oluwamuyiwa <oluwamuyiwadosunmu@gmail.com>
2023-11-16 15:36:54 +0100

    Implemented Restarting a quiz

--- src/components/FinishScreen.jsx
+++ src/components/FinishScreen.jsx
@@ -1,6 +1,6 @@
 import { PropTypes } from "prop-types";
 
-function FinishScreen({ points, maxPossiblePoints, highscore }) {
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
   const percentage = (points / maxPossiblePoints) * 100;
   let emoji;
   if (percentage === 100) emoji = "Medal";
@@ -17,6 +17,12 @@ function FinishScreen({ points, maxPossiblePoints, highscore }) {
         {Math.ceil(percentage)}%)
       </p>
       <p className="highscore">(Highscore: {highscore} points)</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Finish
+      </button>
     </>
   );
 }
@@ -25,6 +31,7 @@ FinishScreen.propTypes = {
   points: PropTypes.number,
   maxPossiblePoints: PropTypes.number,
   highscore: PropTypes.number,
+  dispatch: PropTypes.func,
 };
 
 export default FinishScreen;
